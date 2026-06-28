b1ab806a48ca9cf6edf050b34cdff2ee005462cd
Igor André Guilherme de Miranda <igor.miranda@vsoft.com.br>
2026-06-28 13:20:32 -0300

    feat: ajustes de estrutura para compatibilidade docker

--- run.bat
+++ run.bat
@@ -8,6 +8,7 @@ set PATH=%PATH%;%USERPROFILE%\go\bin
 
 if not exist golang\order mkdir golang\order
 if not exist golang\payment mkdir golang\payment
+if not exist golang\shipping mkdir golang\shipping
 
 protoc --go_out=golang/order --go_opt=paths=source_relative ^
   --go-grpc_out=golang/order --go-grpc_opt=paths=source_relative ^
@@ -17,12 +18,19 @@ protoc --go_out=golang/payment --go_opt=paths=source_relative ^
   --go-grpc_out=golang/payment --go-grpc_opt=paths=source_relative ^
   payment/payment.proto
 
+protoc --go_out=golang/shipping --go_opt=paths=source_relative ^
+  --go-grpc_out=golang/shipping --go-grpc_opt=paths=source_relative ^
+  shipping/shipping.proto
+
 cd golang\order
-go mod init github.com/%GITHUB_USERNAME%/microservices-proto/golang/order
 go mod tidy
 cd ..\..
 
 cd golang\payment
-go mod init github.com/%GITHUB_USERNAME%/microservices-proto/golang/payment
+go mod tidy
+cd ..\..
+
+cd golang\shipping
+go mod init github.com/%GITHUB_USERNAME%/microservices-proto/golang/shipping
 go mod tidy
 cd ..\..
